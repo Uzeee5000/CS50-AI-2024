883edef94391d918be39ae80e3a0599c1411a1c8
Usama Arshad <ua5292@gmail.com>
2024-10-02 17:57:12 +0100

    Added result and winner functions

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -1,7 +1,7 @@
 """
 Tic Tac Toe Player
 """
-
+import copy
 import math
 
 X = "X"
@@ -54,15 +54,66 @@ def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
+    if action not in actions(board):
+        raise NameError("Invalid action")
+    
+    copied_board=copy.deepcopy(board)
+    i,j=action
+    copied_board[i][j]=player(board)
+    return copied_board
 
-    raise NotImplementedError
-
+    
 
 def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
+    
+    # Checking win column-wise
+    for i in range(len(board[0])):
+        all_x = True
+        all_o = True
+        for row in board:
+            if row[i] != X:
+                all_x = False
+            if row[i] != O:
+                all_o = False
+        if all_x:
+            return X
+        if all_o:
+            return O
+
+    # Checking win row-wise
+    for row in board:
+        all_x = True
+        all_o = True
+        for cell in row:
+            if cell != X:
+                all_x = False
+            if cell != O:
+                all_o = False
+        if all_x:
+            return X
+        if all_o:
+            return O
+
+    # Checking win diagonal-wise
+    n = len(board)
+
+    # Check main diagonal (from top-left to bottom-right)
+    if board[0][0] != EMPTY:
+        main_diagonal_winner = board[0][0]
+        if all(board[i][i] == main_diagonal_winner for i in range(n)):
+            return main_diagonal_winner
+
+    # Check anti-diagonal (from top-right to bottom-left)
+    if board[0][n-1] != EMPTY:
+        anti_diagonal_winner = board[0][n-1]
+        if all(board[i][n-1-i] == anti_diagonal_winner for i in range(n)):
+            return anti_diagonal_winner
+
+    # No winner found
+    return None
 
 
 def terminal(board):
